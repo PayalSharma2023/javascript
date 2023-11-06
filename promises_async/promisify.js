76068dbea96e81e9f357ff764740692a45b98551
PayalSharma2023 <payalsharmareal@gmail.com>
2023-11-06 21:00:04 +0530

    updated
    Signed-off-by: PayalSharma2023 <payalsharmareal@gmail.com>

--- promises_async/promisify.js
+++ promises_async/promisify.js
@@ -1,11 +1,22 @@
-//promisify is used when you want to convert a callback function into a promise based function
+//promisify technique involves converting callback-based function into a promise based function
 //with callback comes callbackhell(pyramid of doom) but with promises u can use then to call another function
 //node.js provides utility module for easily converting callback-based functions to promises
 const fs = require("fs");
-const {promisify} = require("util");
+const util = require("util");
 //convert `fs.readFile()` into a function that returns a promise
-const readFilesync = promisify(fs.readFile);
+const readFileAsync = util.promisify(fs.readFile);
+fs.readFile('output.csv','utf8', (err, data)=>{
+    if (err) console.log(err);
+    else console.log(data);
+});
 //you can now use `readFile` with await!
-const buf = await( readFileasync("./package.json"))
-const obj = JSON.parse(buf.toString("utf8"));
-console.log(obj)
+// const buf = await( readFileasync("./package.json"))
+// const obj = JSON.parse(buf.toString("utf8"));
+// console.log(obj)
+readFileAsync("output.csv", 'utf8')
+    .then((text) => {
+        console.log(text);
+    })
+    .catch((err)=>{
+        console.log('Error',err);
+    });
